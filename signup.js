704f43272af63a03f073df622e5dcf04f4725448
aboabdo2002nd-max <aboabdo2002nd@gmail.com>
2026-07-10 03:04:45 +0300

    Add user signup logic with Firebase
    Implement user signup functionality with Firebase authentication and Firestore database integration.

--- signup.js
+++ signup.js
@@ -1,1 +1,51 @@
+import { auth, db } from "./firebase-config.js";
 
+import {
+  createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+  doc,
+  setDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const btn = document.getElementById("signupBtn");
+
+btn.addEventListener("click", async () => {
+
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value;
+  const confirmPassword = document.getElementById("confirmPassword").value;
+
+  if (!name || !email || !password || !confirmPassword) {
+    alert("يرجى تعبئة جميع الحقول");
+    return;
+  }
+
+  if (password !== confirmPassword) {
+    alert("كلمتا المرور غير متطابقتين");
+    return;
+  }
+
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
+    await setDoc(doc(db, "users", userCredential.user.uid), {
+      name: name,
+      email: email,
+      points: 0
+    });
+
+    alert("تم إنشاء الحساب بنجاح");
+    window.location.href = "login.html";
+
+  } catch (error) {
+    alert(error.message);
+  }
+
+});
