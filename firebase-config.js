0efa85ce52c4f94e15b823747ae6c8154ade9bca
aboabdo2002nd-max <aboabdo2002nd@gmail.com>
2026-07-10 03:01:39 +0300

    Add Firebase configuration and initialization

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBOgsGbjz3y2ugybCPo4HXISOBp6JtJ-6c",
+  authDomain: "kurd-rewards.firebaseapp.com",
+  projectId: "kurd-rewards",
+  storageBucket: "kurd-rewards.firebasestorage.app",
+  messagingSenderId: "1021836588474",
+  appId: "1:1021836588474:web:3bece356f0a8b0745d9983",
+  measurementId: "G-201ZQ2VY71"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
